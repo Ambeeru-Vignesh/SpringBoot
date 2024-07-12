3fc611fdf6291d38ab6683166736b08065040bd2
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-12 21:47:45 +0530

    Introduction to SpringBoot

---
+++ src/main/java/com/vignesh/week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,11 @@
+package com.vignesh.week1Introduction.IntroductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+
+    void eatApple() {
+        System.out.println("I am eating an apple!");
+    }
+}
